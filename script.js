ccf4e6f76f08055c71fbfa8e0da6236580509ba1
Kseniya Ushakova <brokilone@yandex.ru>
2020-07-13 15:03:38 +0500

    try scroll blocking

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 $(function () {
+    function disableScroll(){
+        $('html, body').on('mousewheel', function () {
+            $(this).css('overflow', 'hidden');
+        });
+    }
+    function enableScroll(){
+        $('html, body').on('mousewheel', function () {
+            $(this).css('overflow', 'auto');
+        });
+    }
     $('#changebutton')
         .css({
             'margin-left': '20px',
             'margin-bottom': '40px'
         })
         .click( function () {
-            $('#column-left h2').remove();
-            var newElement = $('<h2></h2>');
-            newElement.text('Срочные новости');
-            $('#column-left').prepend(newElement)
+            $('#popup-container').fadeIn(400, disableScroll);
+            $('#popup').animate({
+                width: '200px',
+                height: '300px'
+            }, 400);
         });
+
+    $('#popup-container').click( function (event) {
+        if (event.target == this) {
+            $(this).fadeOut(400, enableScroll);
+            $('#popup').animate({
+                width: 0,
+                height: 0
+            }, 400);
+        }
+    })
 });
